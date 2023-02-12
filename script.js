98de174e923bcb777dc9b1ab3838053ca1afa07f
BrokenRolz <miyukihuang666@gmail.com>
2023-02-12 19:01:08 +0530

    Fix typo

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
-        let cell =- document.createElement('div');
+        let cell = document.createElement('div');
         cell.innerText = (c + 1);
         container.appendChild(cell).className = 'grid-item';
     };
